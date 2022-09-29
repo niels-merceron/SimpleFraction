3fef86f2833d33a801392ff88a32f2eeb0ed82ec
niels-merceron <sipha.2000@gmail.com>
2022-09-29 16:20:14 +0200

    fraction assert

--- main.java
+++ main.java
@@ -3,6 +3,8 @@ public class main {
     public static void main(String[] args) {
 
         Fraction C = new Fraction(4, 5);
+        assert C.GetDenom() > 0;
+        
         System.out.println("denom :" + C.GetDenom() + " num:" + C.Getnum() + "\n");
     }
 }
