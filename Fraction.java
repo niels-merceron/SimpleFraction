87639a40afb49e73d378ce807b104f660e92a506
niels-merceron <sipha.2000@gmail.com>
2022-09-29 15:01:55 +0200

    fraction.3

--- Fraction.java
+++ Fraction.java
@@ -1,6 +1,15 @@
 // Your First Program
 
-public class Fraction{
-    
-}
+public class Fraction {
+    private int denom;
+    private int num;
 
+    public Fraction(int A, int b) {
+        denom = A;
+        num = b;
+    }
+
+    public String toString(){
+        return("\n je suis une fraction \n");
+    }
+}
