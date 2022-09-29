405a002a1045939bd0524e2aa8e235bb9b9695e6
niels-merceron <sipha.2000@gmail.com>
2022-09-29 14:53:59 +0200

    fraction

--- Fraction.java
+++ Fraction.java
@@ -1,7 +1,12 @@
 // Your First Program
 
-class HelloWorld {
+class Fraction{
     public static void main(String[] args) {
         System.out.println("Hello, World!"); 
     }
+}
+
+
+class main{
+    
 }
